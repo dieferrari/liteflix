49d24b7691599f48b595de868e9a96aeb38baa61
Diego Ferrari <dieferrari@diegos-mbp.garbarino.com.ar>
2019-03-24 17:24:37 -0300

    header completed except user dialog

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -6,24 +6,45 @@ class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            searchValue: "",
         }
     }
 
-    
     render() {
         return (
-            <div className="header">
-                <img className="header-logo" src="../../../assets/liteflix_logo.svg" alt="Liteflix logo"/> 
-                <a href="/">Inicio</a>
-                <a href="/series">Series</a>
-                <a href="/peliculas">Películas</a>
-                <a href="/agregados-recientemente">Agregados reciéntemente</a>
-                <a href="/mi-lista">Mi Lista</a>
-                <button>
-                    <img className="header-plus-icon" src="../../../assets/plus_icon.svg" alt="Plus icon"/>
-                    <span>Agregar película</span>
-                </button>
+            <div className="header-container">
+                <div>
+                    <img className="header-logo" src="../../../assets/liteflix_logo.svg" alt="Liteflix logo"/> 
+                    <a href="/">Inicio</a>
+                    <a href="/series">Series</a>
+                    <a href="/peliculas">Películas</a>
+                    <a href="/agregados-recientemente">Agregados reciéntemente</a>
+                    <a href="/mi-lista">Mi Lista</a>
+                    <button className="header-add-button">
+                        <img className="header-plus-icon" src="../../../assets/plus_icon.svg" alt="Plus icon"/>
+                        <span>Agregar película</span>
+                    </button>
+                </div>
+                <div className="header-secondary-container">
+                    <div className="header-search-section">
+                    <form>
+                        <label>
+                        <img src="../../../assets/lens_icon.svg" alt="Lens icon"/> 
+                        <input type="text" placeholder="Títulos, géneros, personas" value={this.state.searchValue} readOnly/>
+                        </label>
+                    </form>
+                    </div>
+                    <a href="/niños">Niños</a>
+                    <button>
+                        <img src="../../../assets/bell_icon.svg" alt="Notifications icon"/> 
+                    </button>
+                    <div className="header-user-container">
+                        <img src="../../../assets/user_icon_1.svg" alt="User icon"/>
+                        <button>
+                            <img src="../../../assets/arrow_icon.svg" alt="Arrow icon"/> 
+                        </button>
+                    </div>
+                </div>
             </div>
         )
     }
