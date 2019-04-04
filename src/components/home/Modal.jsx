d8e3d5a04a5642865a09e41e3a07109b689642a5
Diego Ferrari <dieferrari@diegos-mbp.garbarino.com.ar>
2019-04-04 18:40:14 -0300

    modal WIP

--- src/components/home/Modal.jsx
+++ src/components/home/Modal.jsx
@@ -2,24 +2,48 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
-
+import Input from '@material-ui/core/Input';
+import './Modal.scss'
 
 const styles = theme => ({
-
+    input: {}
 });
 
 class Modal extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            movie_name:"",
+        }
+    }
 
+    inputChangeHandler(event) {
+        this.setState({[event.target.name]: event.target.value})
+    }
 
     render() {
-        const { showModal, closeModalHandler } = this.props
+        const { showModal, closeModalHandler, classes } = this.props
         return (
             <Dialog 
                 open={showModal}	
                 onClose={closeModalHandler}
+                maxWidth="lg"
             >
                 <DialogContent>
-                    HOLIS
+                    <div className="modal-file-drop"><img src="../../../assets/clip_icon.svg" alt="Attachment icon"/><span>Agregar archivo</span> o arrastrarlo y soltarlo aquí</div>
+                    <form action="">
+                    
+                        <Input
+                            
+                            name="movie_name"
+                            value={this.state.movieName}
+                            placeholder="Escribir nombre"
+                            className={classes.input}
+                            onChange={(this.inputChangeHandler.bind(this))}
+                        />
+
+
+                    </form>
                 </DialogContent>
             </Dialog>
         )
